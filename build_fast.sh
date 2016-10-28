f020c7933163758279b68107bfc6312d585fe96d
Jason Hsu <jhsu802701@jasonhsu.com>
2016-10-28 16:12:06 +0000

    Removed from build_fast.sh steps included in creating Docker image; updated sshkit due to error during build

--- build_fast.sh
+++ build_fast.sh
@@ -55,22 +55,6 @@ sudo bash -c "echo '' >> $PG_HBA"
 sudo bash -c "echo 'host    all             all             all                     md5' >> $PG_HBA"
 sudo bash -c "echo '' >> $PG_HBA"
 
-echo '-------------------'
-echo 'sudo apt-get update'
-sudo apt-get update
-
-echo '----------------------------------'
-echo 'sudo apt-get install -y libgtk-3-0'
-sudo apt-get install -y libgtk-3-0
-
-echo '--------------------------------------'
-echo 'sudo apt-get install -y libgtkmm-3.0-1'
-sudo apt-get install -y libgtkmm-3.0-1
-
-echo '----------------------------------'
-echo 'sudo apt-get install -y libnotify4'
-sudo apt-get install -y libnotify4
-
 echo '-------------------------------'
 echo 'sudo service postgresql restart'
 sudo service postgresql restart
